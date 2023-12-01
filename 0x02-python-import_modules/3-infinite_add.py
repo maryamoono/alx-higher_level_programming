d36a0f405cb3160dbc99e060ec685cf18eab3270
Maryam <cakemariam1234@gmail.com>
2023-11-30 17:09:08 -0800

    lol

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import tem
+    import sys
     sum = 0
-    if (len(tem.argv) == 1):
+    if (len(sys.argv) == 1):
         print(0)
     else:
-        for i in range(1, len(tem.argv)):
-            sum += int(tem.argv[i])
+        for i in range(1, len(sys.argv)):
+            sum += int(sys.argv[i])
         print(sum)
