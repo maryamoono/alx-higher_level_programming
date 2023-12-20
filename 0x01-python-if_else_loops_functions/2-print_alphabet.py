130ebad1754e3da47eb16b48f0bb326ba1e8d7fa
Maryam <cakemariam1234@gmail.com>
2023-12-20 14:09:59 -0800

    LOL

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-for ar_co in range(ord('a'), ord('z') + 1):
-    print("{}.format(chr(ar_co)), end='')
+for char_code in range(ord('a'), ord('z') + 1):
+    print("{}".format(chr(char_code)), end='')
