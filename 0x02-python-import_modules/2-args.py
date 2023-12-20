2cc318dc05bacfd3079ea7f8b91968561be09fdb
Maryam <cakemariam1234@gmail.com>
2023-12-20 13:59:58 -0800

    LOL

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    v = len(sys.argv)
-    if (v == 1):
-        print("{} arguments.".format(v - 1))
-    elif (v == 2):
-        print("{} argument:".format(v - 1))
+    n = len(sys.argv)
+    if (n == 1):
+        print("{} arguments.".format(n - 1))
+    elif (n == 2):
+        print("{} argument:".format(n - 1))
         print("1:", sys.argv[1])
     else:
-        print("{} arguments:".format(v - 1))
-        for x in range(1, v):
-            print("{}:".format(x), sys.argv[x])
+        print("{} arguments:".format(n - 1))
+        for i in range(1, n):
+            print("{}:".format(i), sys.argv[i])
