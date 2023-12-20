db9fad58dd29ef6e38f8c469943c23d78e89bf70
Maryam <cakemariam1234@gmail.com>
2023-12-20 14:12:24 -0800

    LOL

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number != 99:
-        print("{:02d}, ".format(number), end='')
-    else:
-        print("{:02d}".format(num))
+for num in range(0, 90):
+    if num % 10 > num / 10:
+        if num != 89:
+            print("{:02d}, ".format(num), end='')
+        else:
+            print("{:02d}".format(num))
