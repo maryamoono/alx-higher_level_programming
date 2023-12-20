f92f72309854395bd503d53682c8e6e8b470229e
Maryam <cakemariam1234@gmail.com>
2023-12-19 17:33:55 -0800

    LOL

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,38 @@
+"""
+This module contains utility functions for handling strings.
+"""
+
+
+class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes a new Square instance with the given size.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size ** 2
