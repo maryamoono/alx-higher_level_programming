7ed2e8166671f6ad890e5574f276c06593a7a8f3
Maryam <cakemariam1234@gmail.com>
2024-03-11 21:58:28 -0700

    LOL

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-consol.log("C is fun");
-consol.log("Python is cool");
-consol.log("JavaScript is amazing);
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing);
